84361b167b4df8c63b25038a536c031c9498d811
Lucas Medeiros <lmedeiros@avenuecode.com>
2018-10-16 17:18:19 -0300

    Update gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-    return gulp.src('src/styles/**/*.scss')
-                .pipe(sass())
-                .pipe(gulp.dest('dist/styles'))
+gulp.task('sass', function() {
+    return gulp
+        .src('src/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('src/styles/**/*.scss');
-})
+gulp.task('watch', function() {
+    gulp.watch('src/styles/**/*.scss', ['sass']);
+});
